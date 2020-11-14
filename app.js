5d5161109284d0f59aed80f543a698e7380f965b
OswaldDB <kevindeanathey@hotmail.com>
2020-11-14 16:32:23 -0600

    making progress

--- app.js
+++ app.js
@@ -1,39 +1,58 @@
 d3.json("samples.json").then(function(data) {
-
-    var example = data.samples[0];
-    delete example.id;
-
-    sortableArray = [];
-    for (i=0; i<example.sample_values.length; i++) {
-        let arrayObj = {sample_value: example.sample_values[i], otu_id: example.otu_ids[i], otu_label: example.otu_labels[i]};
-        sortableArray.push(arrayObj)
-    };
-
-    sortableArray.sort((a, b) => {
-        return b.sample_value - a.sample_value;
-    });
-
-    topTenArray = sortableArray.slice(0,10);
-
-    var sample_values = [];
-    var otu_ids = [];
-    var otu_labels = [];
-    for (i=0; i<10; i++) {
-        sample_values.push(topTenArray[i].sample_value);
-        otu_ids.push("OTU" + topTenArray[i].otu_id);
-        otu_labels.push(topTenArray[i].otu_label);
-    }
-    
-    console.log(sample_values);
-    console.log(otu_ids);
-
-    var trace1 = {
-        x: otu_ids,
-        y: sample_values,
-        type: "bar"
+    // Build the dropdown
+    for (i=0; i<data.samples.length; i++) {
+        d3.select("#selDataset").append("option").text(data.samples[i].id);
     }
 
-    Plotly.newPlot("bar",[trace1])
+    // Set the change conditions
+    d3.selectAll("#selDataset").on("change", updatePloty);
+
+    // Define the id
+    function getId() {
+        var subject_id = d3.selectAll("#selDataset").property("value");
+        for (r=0; r<data.samples.length; r++) {
+            if (data.samples[r].id === subject_id) {
+                return r;
+            }
+        }
+    }
+
+    // Plot bar chart
+    function updatePloty() {
+        //
+        var example = data.samples[getId()];
+        delete example.id;
+        //
+        sortableArray = [];
+        for (i=0; i<example.sample_values.length; i++) {
+            let arrayObj = {sample_value: example.sample_values[i], otu_id: example.otu_ids[i], otu_label: example.otu_labels[i]};
+            sortableArray.push(arrayObj)
+        };
+        //
+        sortableArray.sort((a, b) => {
+            return b.sample_value - a.sample_value;
+        });
+        //
+        topTenArray = sortableArray.slice(0,10);
+        //
+        var sample_values = [];
+        var otu_ids = [];
+        var otu_labels = [];
+        for (i=0; i<10; i++) {
+            sample_values.push(topTenArray[i].sample_value);
+            otu_ids.push("OTU " + topTenArray[i].otu_id);
+            otu_labels.push(topTenArray[i].otu_label);
+        }
+        //
+        var trace1 = {
+            y: otu_ids.reverse(),
+            x: sample_values.reverse(),
+            orientation: 'h',
+            type: "bar"
+        }
+        //
+        Plotly.newPlot("bar",[trace1])
+    }
 
 });
 
