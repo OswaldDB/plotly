2e94b6c80b8ee80d9cdaa327a8077ec52ddfe227
OswaldDB <kevindeanathey@hotmail.com>
2020-11-14 18:34:47 -0600

    2020-11-14 18:34

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
 d3.json("samples.json").then(function(data) {
     // Build the dropdown
-    for (i=0; i<data.samples.length; i++) {
-        d3.select("#selDataset").append("option").text(data.samples[i].id);
+    for (i=0; i<data.names.length; i++) {
+        d3.select("#selDataset").append("option").text(data.names[i]);
     }
+
+    // Initialize the plot
+    updatePloty()
 
     // Set the change conditions to update the bar chart, etc.
     d3.selectAll("#selDataset").on("change", updatePloty);
 
     // Define the id of the subject
     function getId() {
-        for (r=0; r<data.samples.length; r++) {
-            if (data.samples[r].id == d3.selectAll("#selDataset").property("value")) {
+        for (r=0; r<data.names.length; r++) {
+            if (data.names[r] == d3.selectAll("#selDataset").property("value")) {
                 return r;
             }
         }
     }
 
+    // Prepare the demographic info
+    function demoInfo() {
+        d3.select("#sample-metadata").html("");
+        d3.select("#sample-metadata").append("ul");
+        d3.select("ul").append("li").text("id: " + data.metadata[getId()].id);
+        d3.select("ul").append("li").text("ethnicity: " + data.metadata[getId()].ethnicity);
+        d3.select("ul").append("li").text("gender: " + data.metadata[getId()].gender);
+        d3.select("ul").append("li").text("age: " + data.metadata[getId()].age);
+        d3.select("ul").append("li").text("location: " + data.metadata[getId()].location);
+        d3.select("ul").append("li").text("bbtype: " + data.metadata[getId()].bbtype);
+        d3.select("ul").append("li").text("wfreq: " + data.metadata[getId()].wfreq);
+    }
+
+
     // Generate the plot
     function updatePloty() {
+        // Update the demo info box
+        demoInfo()
         // Format the data for the selected subject into a workable, sorted, and sliced form
         var workingArray = [data.samples[getId()].otu_ids,data.samples[getId()].sample_values,data.samples[getId()].otu_labels];
         for (i=0; i<workingArray[0].length; i++) {
@@ -37,6 +56,5 @@ d3.json("samples.json").then(function(data) {
             type: 'bar'
         }
         Plotly.newPlot('bar',[trace])
-    
     }
 })
