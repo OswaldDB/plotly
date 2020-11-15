154e9ce37ff0a093aca181372f34d834f3f69ad3
OswaldDB <kevindeanathey@hotmail.com>
2020-11-14 18:03:14 -0600

    18:03

--- app.js
+++ app.js
@@ -4,55 +4,39 @@ d3.json("samples.json").then(function(data) {
         d3.select("#selDataset").append("option").text(data.samples[i].id);
     }
 
-    // Set the change conditions
+    // Set the change conditions to update the bar chart, etc.
     d3.selectAll("#selDataset").on("change", updatePloty);
 
-    // Define the id
+    // Define the id of the subject
     function getId() {
-        var subject_id = d3.selectAll("#selDataset").property("value");
         for (r=0; r<data.samples.length; r++) {
-            if (data.samples[r].id === subject_id) {
+            if (data.samples[r].id == d3.selectAll("#selDataset").property("value")) {
                 return r;
             }
         }
     }
 
-    // Plot bar chart
+    // Generate the plot
     function updatePloty() {
-        //
-        var example = data.samples[getId()];
-        delete example.id;
-        //
-        sortableArray = [];
-        for (i=0; i<example.sample_values.length; i++) {
-            let arrayObj = {sample_value: example.sample_values[i], otu_id: example.otu_ids[i], otu_label: example.otu_labels[i]};
-            sortableArray.push(arrayObj)
-        };
-        //
-        sortableArray.sort((a, b) => {
-            return b.sample_value - a.sample_value;
-        });
-        //
-        topTenArray = sortableArray.slice(0,10);
-        //
-        var sample_values = [];
-        var otu_ids = [];
-        var otu_labels = [];
+        // Format the data for the selected subject into a workable, sorted, and sliced form
+        var workingArray = [data.samples[getId()].otu_ids,data.samples[getId()].sample_values,data.samples[getId()].otu_labels];
+        for (i=0; i<workingArray[0].length; i++) {
+            workingArray.sort((a, b) => {
+                return b[1] - a[1];
+            })
+        }
+        let slicedArray = [workingArray[0].slice(0,10),workingArray[1].slice(0,10),workingArray[2].slice(0,10)]
         for (i=0; i<10; i++) {
-            sample_values.push(topTenArray[i].sample_value);
-            otu_ids.push("OTU " + topTenArray[i].otu_id);
-            otu_labels.push(topTenArray[i].otu_label);
+            slicedArray[0][i] = "OTU " + slicedArray[0][i]
         }
-        //
-        var trace1 = {
-            y: otu_ids.reverse(),
-            x: sample_values.reverse(),
+        // Generate the plot itself
+        let trace = {
+            x: slicedArray[1].reverse(),
+            y: slicedArray[0].reverse(),
             orientation: 'h',
-            type: "bar"
+            type: 'bar'
         }
-        //
-        Plotly.newPlot("bar",[trace1])
+        Plotly.newPlot('bar',[trace])
+    
     }
-
-});
-
+})
